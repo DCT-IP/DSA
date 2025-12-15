f1ff4a2d406f6ab0f00bde1465755e3b1558789d
DCT <dheemanchaitanyathakur@gmail.com>
2025-12-15 18:23:57 +0530

    another exercise for arrays done

--- Striver/Arrays/Union.cpp
+++ Striver/Arrays/Union.cpp
@@ -7,10 +7,30 @@ Explanation:
 The elements 1, 2 are common to both, 
 3, 4, 5 are from nums1 and 7 is from nums2*/
 #include <iostream>
-int main(){
-    int num1[] = {1, 2, 3, 4, 5}, num2[] = {1, 2, 7};
-    int l1 = sizeof(num1)/sizeof(num1[0]), l2 = sizeof(num2)/sizeof(num2[0]);
-    int op[l1+l2];//maximum possible
-    int act_size = 0;
-    //TBD
+
+bool exists(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++)
+        if (arr[i] == key)
+            return true;
+    return false;
+}
+
+int main() {
+    int A[] = {1, 2, 3, 4, 7};
+    int B[] = {1, 2, 7};
+
+    int n = sizeof(A)/sizeof(int), m = sizeof(B)/sizeof(int);
+    int U[n+m], k = 0;
+
+    for (int i = 0; i < n; i++)
+        U[k++] = A[i];
+
+    for (int i = 0; i < m; i++)
+        if (!exists(U, k, B[i]))
+            U[k++] = B[i];
+
+    for (int i = 0; i < k; i++)
+        std::cout << U[i] << " ";
+
+    return 0;
 }
