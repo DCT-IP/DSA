94d6d1f63877c3a33739d7b93fc467817a62ae0c
DCT <dheemanchaitanyathakur@gmail.com>
2026-01-12 22:31:38 +0530

    did another question of binary search

---
+++ Striver/Arrays/lwrbnd.cpp
@@ -0,0 +1,37 @@
+/*
+Question: Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
+The lower bound algorithm finds the first or the smallest index in a sorted array 
+where the value at that index is greater than or equal to a given key i.e. x.
+
+The lower bound is the smallest index, ind, where arr[ind] >= x. 
+But if any such index is not found, the lower bound algorithm returns n i.e. size of the given array
+
+Example:
+Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+Result: 1
+Explanation: Index 1 is the smallest index such that arr[1] >= x
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[] = {1,2,2,3}, target = 2;
+    int len = sizeof(arr)/sizeof(int);
+    int temp = len-1, start = 0, ans = -1;
+    while(start<=temp){
+        int med = (temp+start)/2;
+        if(arr[med]==target){
+            ans = med;
+            break;
+        } else if(target < arr[med]){
+            temp = med-1;
+        } else {
+            start = med+1;
+        }
+    }
+    if(ans==-1){
+        cout << "not found!" << endl;
+    } else {
+        cout << "found at: "<<ans << endl;
+    }
+}
+//used binary sorting to sort to increase difficulty and due to module named such
--- Striver/README.md
+++ Striver/README.md
@@ -10,6 +10,6 @@ This repository contains my solutions to **Striver's DSA Sheet**, a curated set 
 
 # 🔹 Structure
 
-- Arrays - 12 questions done
+- Arrays - 13 questions done
 
 //To be added more
