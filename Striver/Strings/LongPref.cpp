9e0765ed6fc26675e9930af54a5d501d8f40e67f
DCT <dheemanchaitanyathakur@gmail.com>
2026-01-23 23:13:42 +0530

    will do the qs

---
+++ Striver/Strings/LongPref.cpp
@@ -0,0 +1,17 @@
+/*
+ Write a function to find the longest common prefix string amongst an array of strings. 
+ If there is no common prefix, return an empty string ""
+
+ Input:
+ str = ["flower", "flow", "flight"]
+Output:
+ "fl"
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    vector <string> str= {"flower", "flow", "flight"};
+    //tb done
+}
