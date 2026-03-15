449a0df4d17159c99b3f66f99f347787be76ebca
DCT <dheemanchaitanyathakur@gmail.com>
2026-03-15 23:36:43 +0530

    will complete later

---
+++ Striver/Recursion/combinationsum1.cpp
@@ -0,0 +1,12 @@
+/*
+given an array of distinc int and a target, 
+return the list of all unique combinations where chosen sum is target
+Input: array = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void findCombin(int id, int target, int n);
+int main(){
+    //will doo later
+}
